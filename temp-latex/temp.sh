7644d929e9d5aa68260ac690332228bb65f8e9b5
Pawarit Thareechan <root@lckyn.com>
2021-06-21 08:53:58 +0700

    add temp

--- temp-latex/temp.sh
+++
@@ -1,9 +0,0 @@
-if [ -z "$1" ]
-then
-   echo "No arguments supplied"
-else
-    pdf2svg temp.pdf $1.svg
-    pdftoppm temp.pdf $1 -png -singlefile
-    cp temp.tex $1.tex
-    cp temp.pdf $1.pdf
-fi
